55ecca4ee3d010913d86e8e21dd110a3558aec85
prg-04 <karanjaevans95@gmail.com>
2023-12-14 06:22:33 +0300

    Update greetings component

--- app/javascript/Greetings.js
+++ app/javascript/Greetings.js
@@ -1,16 +1,13 @@
 import React, { useState, useEffect } from "react";
 
 const Greetings = () => {
-  const [greetings, setGreetings] = useState([]);
   const [randomGreeting, setRandomGreeting] = useState("");
 
   useEffect(() => {
     fetch("/greetings.json")
       .then((response) => response.json())
       .then((data) => {
-        setGreetings(data);
-        const randomIndex = Math.floor(Math.random() * data.length);
-        setRandomGreeting(data[randomIndex]?.body || "");
+        setRandomGreeting(data[0]?.body || "");
       })
       .catch((error) => console.error("Error fetching greetings:", error));
   }, []);
